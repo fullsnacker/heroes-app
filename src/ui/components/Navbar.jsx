f0ec4fd5785986a8c1c20bebcf22540f56d04157
fullsnacker <fullsnacker@gmail.com>
2024-02-14 09:54:45 -0300

    useNavigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,13 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+	const navigate = useNavigate();
+
+	const onLogout = () => {
+		navigate('/login', { replace: true });
+		console.log('logout');
+	};
+
 	return (
 		<nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 			<Link className="navbar-brand" to="/">
@@ -32,7 +39,9 @@ export const Navbar = () => {
 			<div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
 				<ul className="navbar-nav ml-auto">
 					<span className="nav-item nav-link text-primary">Fullsnacker</span>
-					<button className="nav-item nav-link btn">Logout</button>
+					<button className="nav-item nav-link btn" onClick={onLogout}>
+						Logout
+					</button>
 				</ul>
 			</div>
 		</nav>
